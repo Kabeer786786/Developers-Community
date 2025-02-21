455813ba83ffd695f4ee033da738777c6ad6ee25
Shaik Kabeer Ahmed <shaikkabeerahmed@786@gmail.com>
2025-02-21 23:15:09 +0530

    Updated Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ import Projects from "./components/home/Projects";
 import Forums from "./components/home/Forums";
 import "./assets/scroller.css";
 
-// Just Change
  
 function App() {
   const navigate = useNavigate();
